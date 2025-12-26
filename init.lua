b798228f40d9f0c63125f8d1ed9e8d81970e5c03
Emre Dikmen <101067129+fabyo0@users.noreply.github.com>
2025-12-26 13:21:45 +0300

    fix: Suppress lspconfig deprecation warnings, update mason config

--- init.lua
+++ init.lua
@@ -5,13 +5,15 @@
 -- Suppress lspconfig deprecation warning
 vim.g.lspconfig_suppress_deprecation = true
 
+-- Suppress ALL lspconfig deprecation warnings
 local original_notify = vim.notify
 vim.notify = function(msg, level, opts)
-  if type(msg) == "string" and msg:match("lspconfig") and msg:match("deprecated") then
+  if type(msg) == "string" and (msg:match("lspconfig") or msg:match("deprecated")) then
     return
   end
   return original_notify(msg, level, opts)
 end
+
 
 -- ===========================================
 -- Full Go IDE Setup for Neovim
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,6 +2,8 @@
 -- Mason: LSP/DAP/Linter Manager
 -- ===========================================
 return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
   {
     "williamboman/mason.nvim",
     build = ":MasonUpdate",
