38836555948b3438a1bb7adb249ad0ce3f20bcca
Emre Dikmen <101067129+fabyo0@users.noreply.github.com>
2025-12-27 23:29:43 +0300

    feat: Add undotree, flash, mini-surround, zen-mode plugins

---
+++ lua/plugins/mini-surround.lua
@@ -0,0 +1,12 @@
+return {
+  "echasnovski/mini.surround",
+  version = "*",
+  event = "VeryLazy",
+  opts = {
+    mappings = {
+      add = "sa",
+      delete = "sd",
+      replace = "sr",
+    },
+  },
+}
---
+++ lua/plugins/zen.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/zen-mode.nvim",
+  keys = {
+    { "<leader>z", "<cmd>ZenMode<CR>", desc = "Zen mode" },
+  },
+  opts = {
+    window = { width = 90 },
+  },
+}
